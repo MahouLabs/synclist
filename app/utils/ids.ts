b34f3244a98f60cac04c542601a69e2b74196e50
Nicolas Chartiot <nicolaschartiot@gmail.com>
2024-04-15 16:07:44 -0300

    Create util for generating ids

---
+++ app/utils/ids.ts
@@ -0,0 +1,10 @@
+import { customAlphabet } from "nanoid";
+export const nanoid = customAlphabet("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz");
+
+const prefixes = {
+  home: "home",
+  cookbook: "ckbk",
+  recipe: "recp",
+} as const;
+
+export const createId = (prefix: keyof typeof prefixes) => [prefixes[prefix], nanoid(20)].join("_");
