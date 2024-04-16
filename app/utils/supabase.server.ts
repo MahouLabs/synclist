8072426aa45334ea20b4541eaf5fbdb556e2510a
Nicolas Chartiot <nicolaschartiot@gmail.com>
2024-04-16 17:13:16 -0300

    Create supabase ts types

--- app/utils/supabase.server.ts
+++ app/utils/supabase.server.ts
@@ -1,12 +1,13 @@
 import type { AppLoadContext } from "@remix-run/cloudflare";
 import { createServerClient, parse, serialize } from "@supabase/ssr";
+import type { Database } from "./supabase.types";
 
 export function createClient(request: Request, context: AppLoadContext) {
   const cookies = parse(request.headers.get("Cookie") ?? "");
   const headers = new Headers();
   const { SUPABASE_URL, SUPABASE_KEY } = context.cloudflare.env;
 
-  return createServerClient(SUPABASE_URL, SUPABASE_KEY, {
+  return createServerClient<Database>(SUPABASE_URL, SUPABASE_KEY, {
     cookies: {
       get(key) {
         return cookies[key];
