65fb360f6c07039cd31a8448c55172cc0ba1d6a3
Willian Alves <wba.alves@gmail.com>
2024-04-19 00:51:12 -0300

    add groceries tables | put operations in order

--- supabase/migrations/20240417223024_bootstrap-tables.sql
+++ supabase/migrations/20240417223024_bootstrap-tables.sql
@@ -1,9 +1,4 @@
-create table "public"."home_members" (
-    "home_id" character varying(25) not null,
-    "user_id" uuid not null,
-    "last_accessed" boolean DEFAULT false
-);
-
+-- home first
 create table "public"."homes" (
     "id" character varying(25) not null,
     "name" text not null,
@@ -11,8 +6,36 @@ create table "public"."homes" (
     "created_at" timestamp with time zone default now() not null
 );
 
+CREATE UNIQUE INDEX homes_pkey ON public.homes USING btree (id);
+
+alter table "public"."homes" add constraint "homes_pkey" PRIMARY KEY using index "homes_pkey";
+
+alter table "public"."homes" add constraint "homes_owner_id_fkey" FOREIGN KEY (owner_id) REFERENCES auth.users(id) not valid;
+
+alter table "public"."homes" validate constraint "homes_owner_id_fkey";
+
 alter table "public"."homes" enable row level security;
 
+create table "public"."home_members" (
+    "home_id" character varying(25) not null,
+    "user_id" uuid not null,
+    "last_accessed" boolean DEFAULT false
+);
+
+CREATE UNIQUE INDEX home_members_pkey ON public.home_members USING btree (home_id, user_id);
+
+-- now home members
+alter table "public"."home_members" add constraint "home_members_pkey" PRIMARY KEY using index "home_members_pkey";
+
+alter table "public"."home_members" add constraint "home_members_home_id_fkey" FOREIGN KEY (home_id) REFERENCES homes(id) not valid;
+
+alter table "public"."home_members" validate constraint "home_members_home_id_fkey";
+
+alter table "public"."home_members" add constraint "home_members_user_id_fkey" FOREIGN KEY (user_id) REFERENCES auth.users(id) not valid;
+
+alter table "public"."home_members" validate constraint "home_members_user_id_fkey";
+
+-- recipes
 create table "public"."recipes" (
     "id" character varying(25) not null,
     "title" character varying(255) not null,
@@ -24,29 +47,9 @@ create table "public"."recipes" (
     "created_at" timestamp with time zone default now() not null
 );
 
-CREATE UNIQUE INDEX home_members_pkey ON public.home_members USING btree (home_id, user_id);
-
-CREATE UNIQUE INDEX homes_pkey ON public.homes USING btree (id);
-
 CREATE UNIQUE INDEX recipes_pkey ON public.recipes USING btree (id);
 
-alter table "public"."home_members" add constraint "home_members_pkey" PRIMARY KEY using index "home_members_pkey";
-
-alter table "public"."homes" add constraint "homes_pkey" PRIMARY KEY using index "homes_pkey";
-
 alter table "public"."recipes" add constraint "recipes_pkey" PRIMARY KEY using index "recipes_pkey";
-
-alter table "public"."home_members" add constraint "home_members_home_id_fkey" FOREIGN KEY (home_id) REFERENCES homes(id) not valid;
-
-alter table "public"."home_members" validate constraint "home_members_home_id_fkey";
-
-alter table "public"."home_members" add constraint "home_members_user_id_fkey" FOREIGN KEY (user_id) REFERENCES auth.users(id) not valid;
-
-alter table "public"."home_members" validate constraint "home_members_user_id_fkey";
-
-alter table "public"."homes" add constraint "homes_owner_id_fkey" FOREIGN KEY (owner_id) REFERENCES auth.users(id) not valid;
-
-alter table "public"."homes" validate constraint "homes_owner_id_fkey";
 
 alter table "public"."recipes" add constraint "recipes_created_by_fkey" FOREIGN KEY (created_by) REFERENCES auth.users(id) not valid;
 
@@ -56,6 +59,42 @@ alter table "public"."recipes" add constraint "recipes_belongs_to_fkey" FOREIGN 
 
 alter table "public"."recipes" validate constraint "recipes_belongs_to_fkey";
 
+-- groceries
+create table "public"."groceries" (
+    "id" character varying(25) not null,
+    "name" character varying(255) not null,
+    "completed" boolean not null,
+    "belongs_to" character varying(25) not null,
+    "created_by" uuid not null,
+    "created_at" timestamp with time zone default now() not null
+);
+
+CREATE UNIQUE INDEX groceries_pkey ON public.groceries USING btree (id);
+
+alter table "public"."groceries" add constraint "groceries_pkey" PRIMARY KEY using index "groceries_pkey";
+
+alter table "public"."groceries" add constraint "groceries_created_by_fkey" FOREIGN KEY (created_by) REFERENCES auth.users(id) not valid;
+
+alter table "public"."groceries" validate constraint "groceries_created_by_fkey";
+
+alter table "public"."groceries" validate constraint "groceries_belongs_to_fkey";
+
+-- groceries_recipes
+create table "public"."groceries_recipes" (
+    "grocery_id" character varying(25) not null,
+    "recipe_id" character varying(25) not null,
+    "amount" integer not null
+);
+
+CREATE UNIQUE INDEX groceries_recipes_pkey ON public.groceries_recipes USING btree (grocery_id, recipe_id);
+
+alter table "public"."groceries_recipes" add constraint "groceries_recipes_pkey" PRIMARY KEY using index "groceries_recipes_pkey";
+
+alter table "public"."groceries_recipes" validate constraint "groceries_recipes_grocery_id_fkey";
+
+alter table "public"."groceries_recipes" validate constraint "groceries_recipes_recipe_id_fkey";
+
+-- permissions
 grant delete on table "public"."home_members" to "anon";
 
 grant insert on table "public"."home_members" to "anon";
