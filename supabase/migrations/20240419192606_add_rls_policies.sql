ea9d7b03a85d2e6846f242e1d38cbad848e13aac
Nicolas Chartiot <nicolaschartiot@gmail.com>
2024-04-19 16:26:52 -0300

    Create migration for adding rls policies

---
+++ supabase/migrations/20240419192606_add_rls_policies.sql
@@ -0,0 +1,162 @@
+create policy "Enable insert for authenticated users only"
+on "public"."groceries"
+as permissive
+for insert
+to authenticated
+with check (true);
+
+
+create policy "Enable select for authenticated users only"
+on "public"."groceries"
+as permissive
+for select
+to authenticated
+using (true);
+
+
+create policy "Enable insert for authenticated users only"
+on "public"."home_members"
+as permissive
+for insert
+to authenticated
+with check (true);
+
+
+create policy "Enable select for authenticated users only"
+on "public"."home_members"
+as permissive
+for select
+to authenticated
+using (true);
+
+
+create policy "Enable insert for authenticated users only"
+on "public"."homes"
+as permissive
+for insert
+to authenticated
+with check (true);
+
+
+create policy "Enable select for authenticated users only"
+on "public"."homes"
+as permissive
+for select
+to authenticated
+using (true);
+
+
+create policy "Enable insert for authenticated users only"
+on "public"."items"
+as permissive
+for insert
+to authenticated
+with check (true);
+
+
+create policy "Enable select for authenticated users only"
+on "public"."items"
+as permissive
+for select
+to authenticated
+using (true);
+
+
+create policy "Enable insert for authenticated users only"
+on "public"."recipes"
+as permissive
+for insert
+to authenticated
+with check (true);
+
+
+create policy "Enable select for authenticated users only"
+on "public"."recipes"
+as permissive
+for select
+to authenticated
+using (true);
+
+
+create policy "Enable insert for authenticated users only"
+on "public"."recipes_items"
+as permissive
+for insert
+to authenticated
+with check (true);
+
+
+create policy "Enable select for authenticated users only"
+on "public"."recipes_items"
+as permissive
+for select
+to authenticated
+using (true);
+
+
+create policy "Enable insert for authenticated users only"
+on "public"."recipes_tags"
+as permissive
+for insert
+to authenticated
+with check (true);
+
+
+create policy "Enable select for authenticated users only"
+on "public"."recipes_tags"
+as permissive
+for select
+to authenticated
+using (true);
+
+
+create policy "Enable insert for authenticated users only"
+on "public"."schedules"
+as permissive
+for insert
+to authenticated
+with check (true);
+
+
+create policy "Enable select for authenticated users only"
+on "public"."schedules"
+as permissive
+for select
+to authenticated
+using (true);
+
+
+create policy "Enable insert for authenticated users only"
+on "public"."schedules_recipes"
+as permissive
+for insert
+to authenticated
+with check (true);
+
+
+create policy "Enable select for authenticated users only"
+on "public"."schedules_recipes"
+as permissive
+for select
+to authenticated
+using (true);
+
+
+create policy "Enable insert for authenticated users only"
+on "public"."tags"
+as permissive
+for insert
+to authenticated
+with check (true);
+
+
+create policy "Enable select for authenticated users only"
+on "public"."tags"
+as permissive
+for select
+to authenticated
+using (true);
+
+
+
+
