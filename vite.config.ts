5038b9c60c9e4e36ba0bb7bdf2137280b51efa7b
Nicolas Chartiot <nicolaschartiot@gmail.com>
2024-04-20 14:34:45 -0300

    Remove email sign in since that doesn't work with cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  ssr: {
-    external: ["@supabase/ssr"],
-  },
   plugins: [
     remixCloudflareDevProxy(),
     remixDevTools(),
